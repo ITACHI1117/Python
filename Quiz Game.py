d145094fbdeb3ee70e48934316337f02ce0b2cbb
ITACHI1117 <ajogujoseph0317@gmail.com>
2023-06-20 15:11:54 +0100

    completed the rock, paper, scissors task currently working onn the quiz task

---
+++ Quiz Game.py
@@ -0,0 +1,22 @@
+def new_game():
+  pass
+
+def check_answer():
+    pass
+
+def display_score():
+    pass
+
+def play_again():
+    pass
+
+questions = {
+    "Who created Python?: ": "A",
+    "What year was python created?: " : "B",
+    "Python is tribute to which comedy group?: ": "C",
+    "Is the Earth round?: ": "A"
+}
+
+options = {
+    [["A. Guido"]]
+}
